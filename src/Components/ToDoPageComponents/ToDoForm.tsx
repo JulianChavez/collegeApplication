eab6866c0f534a15409d79b78ceed4865049e025
JulianChavez <julianchavez13@gmail.com>
2024-01-21 13:40:35 -0800

    Added Form

--- src/Components/ToDoPageComponents/ToDoForm.tsx
+++ src/Components/ToDoPageComponents/ToDoForm.tsx
@@ -1,17 +1,36 @@
+import { useRef } from 'react';
+
 import Card from "../UI/Card";
 import classes from './ToDoForm.module.css'
+import React from "react";
 
 function ToDoForm() {
+    const toDoInputRef = useRef<HTMLInputElement>(null);
+
+
+    function submitHandler(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        //The ! tells typescript to say that during the time of operation current would not be NULL
+        const enteredToDo = toDoInputRef.current!.value;
+        const ToDoFormData = {
+            task: enteredToDo
+        }
+
+        console.log(ToDoFormData)
+
+
+    }
     return (
         <Card>
-            <form className={classes.form}>
+            <form className={classes.form} onSubmit={submitHandler}>
                 <div>
                     <label htmlFor="title"> To Do List</label>
-                    <input type="text" required id="title" />
+                    <input type="text" required id="title" ref={toDoInputRef} />
                 </div>
-
+                <div >
+                    <button>Add</button>
+                </div>
             </form>
-
         </Card>
     )
 
